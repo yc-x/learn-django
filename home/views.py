ebf70b83334b933f96220831e5bb92bea103252e
yc-x <34796396+yc-x@users.noreply.github.com>
2022-07-26 23:40:18 -0700

    Add html page with templates

--- home/views.py
+++ home/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    return render(request, 'home/Hello.html', {'today': datetime.today()})
 
 def initial(request):
     return HttpResponse('Hello, World!!')
