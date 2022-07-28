2730dd1f1a124f0381ee453c1e225baefde2e324
yc-x <34796396+yc-x@users.noreply.github.com>
2022-07-27 20:55:45 -0700

    Add superuser and authentication part

--- home/views.py
+++ home/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def home(request):
     return render(request, 'home/Hello.html', {'today': datetime.today()})
 
+# initial screen
 def initial(request):
     return HttpResponse('Hello, World!!')
+
+@login_required
+def authorized(request):
+    return render(request, 'home/authorized.html')
