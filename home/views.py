1e891737bda5e16896e7b7c909ac09c88153e6f6
yc-x <34796396+yc-x@users.noreply.github.com>
2022-08-01 21:39:51 -0700

    Modified views to class-based template

--- home/views.py
+++ home/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
 # Create your views here.
 
-def home(request):
-    return render(request, 'home/Hello.html', {'today': datetime.today()})
+class HomeView(TemplateView):
+    template_name = 'home/Hello.html'
+    extra_context = {'today': datetime.today()}
+
+class AuthorizedView(LoginRequiredMixin, TemplateView):
+    template_name = 'home/authorized.html'
+    login_url = '/admin'
+
+# def home(request):
+#     return render(request, 'home/Hello.html', {'today': datetime.today()})
 
 # initial screen
 def initial(request):
     return HttpResponse('Hello, World!!')
 
-@login_required(login_url='/admin')
-def authorized(request):
-    return render(request, 'home/authorized.html')
+# @login_required(login_url='/admin')
+# def authorized(request):
+#     return render(request, 'home/authorized.html')
--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
+from django.http import Http404
+from django.views.generic import ListView, DetailView
 from .models import Notes
 
-# Create your views here.
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
+# Create your views here.
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"   # here defines the name of context dicitonary
+    template_name = "notes/notes_list.html"
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+    template_name = "notes/notes_detail.html"
+    
+# def list(request):
+#     all_notes = Notes.objects.all()
+#     return render(request, 'notes/notes_list.html', {'notes': all_notes})
+
+# def detail(request, pk):
+#     try:
+#         note = Notes.objects.get(pk = pk)
+#     except Notes.DoesNotExist:
+#         raise Http404("Note doesn't exist")
+#     return render(request, 'notes/notes_detail.html', {'note': note})
