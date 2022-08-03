d0c1d5e59ba9b6d9651991e40f29fd18a18b9257
yc-x <34796396+yc-x@users.noreply.github.com>
2022-08-03 01:15:46 -0700

    Add update and delete for notes

--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('notes', views.NotesListView.as_view(), name="notes.list"),
-    path('notes/<int:pk>', views.NotesDetailView.as_view(), name="notes.detail"),
-    path('notes/new', views.NotesCreateView.as_view(), name="notes.new"),
+    path('notes/<int:pk>', views.NotesDetailView.as_view(), name="notes.detail"),   # read
+    path('notes/new', views.NotesCreateView.as_view(), name="notes.new"),   # create
+    path('notes/<int:pk>/edit', views.NotesUpdateView.as_view(), name="notes.update"),   # update
+    path('notes/<int:pk>/delete', views.NotesDeleteView.as_view(), name="notes.delete"),    # delete
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic.edit import DeleteView
+
 from .models import Notes
 from .forms import NotesForm
+
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name = "notes/notes_delete.html"
+
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
 
 class NotesCreateView(CreateView):
     model = Notes
